fef075ec392b88c5b9a20bb2f194be2a6c158b01
Srini Surabathini <surabathini@hotmail.com>
2025-02-26 10:15:10 +0000

    add args

--- pgtest/reassign_ownership.sh
+++ pgtest/reassign_ownership.sh
@@ -1,15 +1,24 @@
 #!/bin/bash
 
+# Check if all arguments are provided
+if [ "$#" -ne 3 ]; then
+    echo "Usage: $0 <host_name> <old_owner> <new_owner>"
+    exit 1
+fi
+
+# Export environment variables
+# export PGUSER=
+# export PGPASSWORD=
+
 # Set the psql command
-export PGUSER=
-export PGPASSWORD=
+HOST_NAME=$1
+OLD_OWNER=$2
+NEW_OWNER=$3
+
 DEFAULT_DB="postgres"
 PORT=5432
+export PGHOST=$HOST_NAME
 PSQL_CMD="psql -d $DEFAULT_DB -p $PORT"
-
-# Set the old and new owner
-OLD_OWNER="a"
-NEW_OWNER="b"
 
 # Get the list of databases
 DATABASES=$($PSQL_CMD -t -A -c "SELECT datname FROM pg_database WHERE datistemplate = false;")
